8d0b693dda36c1635ea490e7e0fb219856dd8ec9
cfrehr <cfrehr@gmail.com>
2016-09-16 20:59:04 -0500

    Update parallaxTest.js

--- parallaxTest.js
+++ parallaxTest.js
@@ -1,6 +1,6 @@
 (function() {
     var parallax = document.querySelectorAll(".parallax"),
-        speed = -0.58;
+        speed = 0.58;
     
     window.onscroll = function() {
         [].slice.call(parallax).forEach(function(el,i) {
