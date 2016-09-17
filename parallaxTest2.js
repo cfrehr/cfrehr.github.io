314e2c2fb149dbb33b859ec6827cf6eeee0a7965
cfrehr <cfrehr@gmail.com>
2016-09-17 13:52:09 -0500

    Update parallaxTest2.js

--- parallaxTest2.js
+++ parallaxTest2.js
@@ -19,6 +19,14 @@
       windowWidth = document.documentElement.clientWidth;
       windowRatio = windowWidth / windowHeight;
       
+      // Note 1:    Finding document sizes are a browser compatability issue; every browser has it's own standards and quirks.
+      //            For example, Firefox may interpret the height of a document as document.body.clientHeight while Opera uses
+      //            document.documentElement.clientHeight. Every browser calculates document dimensions in a different way.
+      //                http://www.webdeveloper.com/forum/showthread.php?179931-document-documentElement-vs-document-body
+      // Note 2:    There is no single, magic-solution to finding a document's dimensions, so instead, you have to find the 
+      //            maximum value of all possible browser-specific calls. This will then find the correct document dimension
+      //            given the user's browser specs.
+      //                http://stackoverflow.com/questions/1145850/how-to-get-height-of-entire-document-with-javascript
       docHeight = Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
       
       maxShiftDistance = (windowWidth * inversebgRatio) - windowHeight;
